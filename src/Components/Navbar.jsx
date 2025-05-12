2370b562b54b7b0340d258c66542cea2b1c33a45
Shayan Amir <sshayan.aamir@gmail.com>
2025-05-12 18:56:38 +0500

    Fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <div className="navbar navbar-expand-xl bg-white px-3 px-xl-4 py-4">
       <div className="container-fluid ">
-        <Link to="/" className="text-decoration-none col-6 col-sm-auto">
+        <Link to="" className="text-decoration-none col-6 col-sm-auto">
           <h1 className="text-Poppins fw-600 text-black fs-4 mb-0">
             Construction Company Landing
           </h1>
@@ -25,47 +25,47 @@ function Navbar() {
           id="CollapsableNavbar"
           className="nav justify-content-end me-4 collapse navbar-collapse gap-4 align-content-center"
         >
-          <Link to="/Project" className="text-decoration-none">
+          <Link to="Project" className="text-decoration-none">
             <p className="text-Poppins fw-medium fs-14px text-black mb-0">
               Project
             </p>
           </Link>
-          <Link to="/Blog" className="text-decoration-none">
+          <Link to="Blog" className="text-decoration-none">
             <p className="text-Poppins fw-medium fs-14px text-black mb-0">
               Blog
             </p>
           </Link>
-          <Link to="/Portfolio" className="text-decoration-none">
+          <Link to="Portfolio" className="text-decoration-none">
             <p className="text-Poppins fw-medium fs-14px text-black mb-0">
               Portfolio
             </p>
           </Link>
-          <Link to="/Service" className="text-decoration-none">
+          <Link to="Service" className="text-decoration-none">
             <p className="text-Poppins fw-medium fs-14px text-black mb-0">
               Service
             </p>
           </Link>
-          <Link to="/Services" className="text-decoration-none">
+          <Link to="Services" className="text-decoration-none">
             <p className="text-Poppins fw-medium fs-14px text-black mb-0">
               Services
             </p>
           </Link>
-          <Link to="/Contact" className="text-decoration-none">
+          <Link to="Contact" className="text-decoration-none">
             <p className="text-Poppins fw-medium fs-14px text-black mb-0">
               Contact
             </p>
           </Link>
-          <Link to="/About" className="text-decoration-none">
+          <Link to="About" className="text-decoration-none">
             <p className="text-Poppins fw-medium fs-14px text-black mb-0">
               About
             </p>
           </Link>
-          <Link to="/Home" className="text-decoration-none">
+          <Link to="Home" className="text-decoration-none">
             <p className="text-Poppins fw-medium fs-14px text-black mb-0">
               Home
             </p>
           </Link>
-          <Link to="/Cart" className="text-decoration-none">
+          <Link to="Cart" className="text-decoration-none">
             <img src={Cart} alt="" />
           </Link>
         </div>
